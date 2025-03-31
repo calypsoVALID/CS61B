2c3a5c03c90bd4dbf1e48d8815c979ffac6f1ed0
calypsoVALID <1105340032@qq.com>
2025-03-31 08:41:53 +0800

    Finished PalindromeStage Debug

--- lab03/src/adventure/SpeciesListStage.java
+++ lab03/src/adventure/SpeciesListStage.java
@@ -101,6 +101,10 @@ public class SpeciesListStage implements AdventureStage {
                 copy.remove(o);
             }
         }
-        return similarObjects / listOne.size();
+        if (listOne.size() == 0 || listTwo.size() == 0) {
+            return similarObjects;
+        } else {
+            return similarObjects / listOne.size();
+        }
     }
 }
