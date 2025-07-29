8f0895fafab4d5fef81b277c827c38b1e6d5689d
calypsoVALID <1105340032@qq.com>
2025-07-29 15:56:55 +0800

    Finished lab07

--- lab07/src/hashmap/MyHashMap.java
+++ lab07/src/hashmap/MyHashMap.java
@@ -1,6 +1,6 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  *  A hash table-backed Map implementation.
@@ -9,6 +9,172 @@ import java.util.Collection;
  *  @author YOUR NAME HERE
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map already contains the specified key, replaces the key's mapping
+     * with the value specified.
+     *
+     * @param key
+     * @param value
+     */
+    @Override
+    public void put(K key, V value) {
+        if (key == null) {
+            throw new NullPointerException("key can not be null");
+        }
+        int index = Math.floorMod(key.hashCode(), buckets.length);
+
+        // 遍历buckets的每个node如果key有相同，则更新value
+        for (Node node: buckets[index]) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+        }
+
+        // 如果没有则新建
+        buckets[index].add(new Node(key, value));
+        size++;
+
+        if ((double) size / buckets.length > loadFactor) {
+            resize(buckets.length * 2);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize(int newSize) {
+        Collection<Node>[] oldBuckets = buckets;
+        buckets = (Collection<Node>[]) new Collection[newSize];
+
+        // 初始化新buckets
+        for (int i = 0; i < newSize; i++) {
+            buckets[i] = createBucket();
+        }
+
+        size = 0;// 重修
+
+        // 重新put所有旧节点
+        for (Collection<Node> bucket : oldBuckets) {
+            for (Node node : bucket) {
+                put(node.key, node.value);
+            }
+        }
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     *
+     * @param key
+     */
+    @Override
+    public V get(K key) {
+        if (key == null) {
+            throw new NullPointerException("key can not be null");
+        }
+        int index = Math.floorMod(key.hashCode(), buckets.length);
+        for (Node node: buckets[index]) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns whether this map contains a mapping for the specified key.
+     *
+     * @param key
+     */
+    @Override
+    public boolean containsKey(K key) {
+        if (key == null) {
+            return false;
+        }
+        int index = Math.floorMod(key.hashCode(), buckets.length);
+        for (Node node: buckets[index]) {
+            if (node.key.equals(key)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     */
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    /**
+     * Removes every mapping from this map.
+     */
+    @Override
+    public void clear() {
+        for (Collection<Node> bucket : buckets) {
+            bucket.clear();
+        }
+        size = 0;
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map. Not required for this lab.
+     * If you don't implement this, throw an UnsupportedOperationException.
+     */
+    @Override
+    public Set<K> keySet() {
+        Set<K> keys = new HashSet<>();
+        for (Collection<Node> bucket : buckets) {
+            for (Node node : bucket) {
+                keys.add(node.key);
+            }
+        }
+        return keys;
+    }
+
+    /**
+     * Removes the mapping for the specified key from this map if present,
+     * or null if there is no such mapping.
+     * Not required for this lab. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     *
+     * @param key
+     */
+    @Override
+    public V remove(K key) {
+        if (key == null) {
+            throw new NullPointerException("key can not be null");
+        }
+
+        int index = Math.floorMod(key.hashCode(), buckets.length);
+        Iterator<Node> iter = buckets[index].iterator();
+
+        while (iter.hasNext()) {
+            Node node = iter.next();
+            if (node.key.equals(key)) {
+                V value = node.value;
+                iter.remove();
+                size--;
+                return value;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
 
     /**
      * Protected helper class to store key/value pairs
@@ -25,13 +191,21 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     }
 
     /* Instance Variables */
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
     private Collection<Node>[] buckets;
+    private int size;
+    private double loadFactor;
     // You should probably define some more!
 
     /** Constructors */
-    public MyHashMap() { }
+    public MyHashMap() {
+        this(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR);
+    }
 
-    public MyHashMap(int initialCapacity) { }
+    public MyHashMap(int initialCapacity) {
+        this(initialCapacity, DEFAULT_LOAD_FACTOR);
+    }
 
     /**
      * MyHashMap constructor that creates a backing array of initialCapacity.
@@ -40,7 +214,23 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialCapacity initial size of backing array
      * @param loadFactor maximum load factor
      */
-    public MyHashMap(int initialCapacity, double loadFactor) { }
+    @SuppressWarnings("unchecked")
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        if (initialCapacity < 1) {
+            throw new IllegalArgumentException("Capacity must be >= 1");
+        }
+        if (loadFactor <= 0) {
+            throw new IllegalArgumentException("Load factor must be > 0");
+        }
+        this.loadFactor = loadFactor;
+        this.buckets = (Collection<Node>[]) new Collection[initialCapacity];// 创建桶数组
+        this.size = 0;
+
+        // 用createBucket()初始化每个桶
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = createBucket();
+        }
+    }
 
     /**
      * Returns a data structure to be a hash table bucket
@@ -63,8 +253,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        // TODO: Fill in this method.
-        return null;
+        return new LinkedList<>();
     }
 
     // TODO: Implement the methods of the Map61B Interface below
